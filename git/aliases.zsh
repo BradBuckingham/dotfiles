c9eacabbcde400208d6786c5502de5bddefcf2f5
Brad Buckingham <eljefegrande@gmail.com>
2012-05-05 20:59:43 -0700

    Updated some git aliases (we'll see how long these last...)

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,9 +1,14 @@
-alias gs='git status'
+alias gs='git status --short --branch'
+alias ga='git add'
+alias gc='git commit'
+alias gcm='git commit -m '
+
 alias gl='git log --oneline --decorate --graph'
 alias gla='git log --oneline --decorate --graph --all'
 alias gls='git log --oneline --decorate --graph --stat'
 alias gll='git log --decorate --graph'
 alias glls='git log --decorate --graph --stat'
 alias glla='git log --decorate --graph --all'
+
 alias gd='git diff'
 alias gds='git diff --staged'
