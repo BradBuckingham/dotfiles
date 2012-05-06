7b6aacc14ca7453afb1900d9aed0210d7a8dc320
Brad Buckingham <eljefegrande@gmail.com>
2012-05-05 18:17:45 -0700

    Added system aliases

---
+++ system/aliases.zsh
@@ -0,0 +1,9 @@
+alias ls='ls -G'
+alias ll='ls -alh'
+
+alias u='cd .. && ls'
+alias uu='cd ../.. && ls'
+alias uuu='cd ../../.. && ls'
+alias uuuu='cd ../../../.. && ls'
+
+alias grep='grep --color=auto'
