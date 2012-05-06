8112c240ebfb5d7fc095281b0d1331c288adb8ce
Brad Buckingham <eljefegrande@gmail.com>
2012-05-06 05:04:22 -0700

    Added MacVim's helpful mvim script to bin/

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
 # Prepending '/usr/local/bin' to PATH (for homebrew). For systems without
 # homebrew installed (Linux, for example) this change should be innocuous.
-export PATH="/usr/local/bin:$PATH"
+#
+# Also, prepending '$DOTFILES/bin' so that anything in dotfiles' bin
+# directory is automatically available.
+export PATH="/usr/local/bin:$DOTFILES/bin:$PATH"
