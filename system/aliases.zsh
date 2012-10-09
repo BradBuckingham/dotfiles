bfa94201a8acf56c378600ca4aa207077fa3cd2b
Brad Buckingham <eljefegrande@gmail.com>
2012-10-08 21:58:51 -0700

    zsh: Enable OS-specific ls colors

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,4 +1,15 @@
-alias ls='ls -G'
+# Tell ls to colorize its output. Due to the fact that ls' colorize-output
+# option is OS-specific, I decide which colorize-output option to use via
+# Bash's OSTYPE (which also works in ZSH).
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # OSX (no coreutils)
+  alias ls='ls -G'
+
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  # Standard Linux (Ubuntu, Fedora, etc) with coreutils' ls
+  alias ls='ls --color=auto'
+fi
+
 alias ll='ls -alh'
 
 alias u='cd .. && ls'
