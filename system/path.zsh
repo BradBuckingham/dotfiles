43f6f1bc40c5655d0c3c135e0a64add63c458b94
Brad Buckingham <bradbuckingham@gmail.com>
2024-03-03 14:14:20 -0800

    Fix Homebrew installation directory (in PATH) for Apple Silicon Macs

--- system/path.zsh
+++ system/path.zsh
@@ -1,6 +1,7 @@
-# Prepending '/usr/local/bin' to PATH (for homebrew). For systems without
-# homebrew installed (Linux, for example) this change should be innocuous.
+# Prepending '/opt/homebrew/bin' to PATH for Homebrew on Apple Silicon Macs
+# and '/usr/local/bin' for Homebrew on Intel Macs. For systems without
+# homebrew installed (Linux, for example) this should be innocuous.
 #
 # Also, prepending '$DOTFILES/bin' so that anything in dotfiles' bin
 # directory is automatically available.
-export PATH="/usr/local/bin:$DOTFILES/bin:$PATH"
+export PATH="/opt/homebrew/bin:/usr/local/bin:$DOTFILES/bin:$PATH"
